2ae3c6ba239e14a22f46ee111f17a289e1e7db9b
Romain <romain@cozycloud.cc>
2016-10-27 16:09:14 +0200

    standard test file

--- test/unit/crud.js
+++ test/unit/crud.js
@@ -1,15 +1,17 @@
-import fetchMock from 'fetch-mock'
+/* eslint-env mocha */
+
+// eslint-disable-next-line no-unused-vars
 import should from 'should'
+
 import cozy from '../../src'
+import fetchMock from 'fetch-mock'
 
-describe('Fetch document', function(){
-
-  before(() => fetchMock.get('/data/io.cozy.testobject/42', {"answer": 42}))
+describe('Fetch document', function () {
+  before(() => fetchMock.get('/data/io.cozy.testobject/42', {'answer': 42}))
   after(() => fetchMock.restore())
 
-  it("Call the proper route", async function(){
-    let fetched = await cozy.find("io.cozy.testobject", "42")
-    fetched.should.have.property("answer", 42)
-  });
-
-});
+  it('Call the proper route', async function () {
+    let fetched = await cozy.find('io.cozy.testobject', '42')
+    fetched.should.have.property('answer', 42)
+  })
+})
