c51f50fa5d70de8b2b22588e675f2ee408c7ed55
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2017-01-24 17:22:58 +0100

    Add settings namespace and diskUsage method

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import * as crud from './crud'
 import * as mango from './mango'
 import * as files from './files'
 import * as offline from './offline'
+import * as settings from './settings'
 
 const {AccessToken: AccessTokenV3, Client: ClientV3} = auth
 
@@ -77,10 +78,15 @@ const offlineProto = {
   stopSync: offline.stopSync
 }
 
+const settingsProto = {
+  diskUsage: settings.diskUsage
+}
+
 class Cozy {
   constructor (options) {
     this.files = {}
     this.offline = {}
+    this.settings = {}
     this.auth = {
       Client: ClientV3,
       AccessToken: AccessTokenV3,
@@ -127,6 +133,7 @@ class Cozy {
     addToProto(this, this.auth, authProto, disablePromises)
     addToProto(this, this.files, filesProto, disablePromises)
     addToProto(this, this.offline, offlineProto, disablePromises)
+    addToProto(this, this.settings, settingsProto, disablePromises)
 
     if (options.offline) {
       this.offline.init(options.offline)
