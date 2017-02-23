4a48fd829c13b523e1fc522279a79f68b2a74072
Enguerran <enguerran@ticabri.com>
2017-02-23 18:53:45 +0100

    fix travis tests

--- test/integration/offline.js
+++ test/integration/offline.js
@@ -20,6 +20,10 @@ let docs = [
   {group: 'B', path: '/a/b/d', year: 2104}
 ]
 
+function sleep (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 describe('offline', function () {
   const cozy = {}
 
@@ -31,17 +35,13 @@ describe('offline', function () {
       cozyURL: COZY_STACK_URL,
       token: COZY_STACK_TOKEN
     })
-    for (var i = 0, l = docs.length; i < l; i++) {
-      docs[i] = await cozy.client.data.create(DOCTYPE, docs[i])
-    }
+    docs = await Promise.all(docs.map(doc => cozy.client.data.create(DOCTYPE, doc)))
   })
 
   after(async function () {
     if (COZY_STACK_VERSION === '3') {
-      for (var i = 0, l = docs.length; i < l; i++) {
-        await cozy.client.data.delete(DOCTYPE, docs[i])
-      }
-      cozy.client.offline.stopAllSync()
+      await docs.forEach(doc => cozy.client.data.delete(DOCTYPE, doc))
+      cozy.client.offline.stopAllSync('after')
     }
   })
 
@@ -53,73 +53,55 @@ describe('offline', function () {
     complete.docs_written.should.equal(0)
   }).timeout(3 * 1000)
 
-  it('can\'t replication with live option.', async function () {
-    let err = null
-
-    try {
-      cozy.client.offline.createDatabase(DOCTYPE, {adapter: 'memory'})
-      await cozy.client.offline.replicateFromCozy(DOCTYPE, {live: true})
-    } catch (e) {
-      err = e
-    } finally {
-      err.should.eql(new Error('You can\'t use `live` option with Cozy couchdb.'))
-    }
+  it('can\'t replicate with live option.', async function () {
+    cozy.client.offline.createDatabase(DOCTYPE, {adapter: 'memory'})
+    return cozy.client.offline.replicateFromCozy(DOCTYPE, {live: true}).should.be.rejectedWith({ message: 'You can\'t use `live` option with Cozy couchdb.' })
   })
 
-  it('can auto replicate database', async function () {
+  it('can\'t replicate without adding the correct doctype.', async function () {
+    cozy.client.offline.createDatabase(DOCTYPE, {adapter: 'memory'})
+    const wrongDoctype = 'another.doctype'
+    return cozy.client.offline.replicateFromCozy(wrongDoctype).should.be.rejectedWith({ message: `You should add this doctype: ${wrongDoctype} to offline.` })
+  })
+
+  it('can\'t replicate without creating the database in offline.', async function () {
+    const someDoctype = 'some.doctype'
+    return cozy.client.offline.replicateFromCozy(someDoctype).should.be.rejectedWith({ message: `You should add this doctype: ${someDoctype} to offline.` })
+  })
+
+  it('can replicate created object in local database', async function () {
     // create a database
-    let db = cozy.client.offline.createDatabase(DOCTYPE, {adapter: 'memory'})
-    const lastSeq = await new Promise((resolve) => {
-      // replicate data from gozy
-      cozy.client.offline.replicateFromCozy(DOCTYPE, {}, {complete: (info) => {
-        db.changes().on('complete', (info) => {
-          resolve(info.last_seq)
-        })
-      }})
-    })
-
-    let count = 0
-    db.changes({live: true, include_docs: true, since: lastSeq})
-      .on('change', (change) => { count++ })
-
-    const smallDoc = {test: 187}
-    // activate synchronisation every 3000ms
-    cozy.client.offline.startSync(DOCTYPE, 3)
-    await new Promise(resolve => setTimeout(resolve, 1 * 1000))
-
+    const db = cozy.client.offline.createDatabase(DOCTYPE, { adapter: 'memory' })
     // create a doc
-    let doc = await cozy.client.data.create(DOCTYPE, smallDoc)
-    const docId = doc._id
+    const sampleDoc = { data: 'some Data' }
+    const remoteDoc = await cozy.client.data.create(DOCTYPE, sampleDoc)
+    // check the db to look for the new doc
+    db.get(remoteDoc._id).should.be.rejectedWith({ message: 'missing' })
+    // replicate the database
+    await cozy.client.offline.replicateFromCozy(DOCTYPE)
+    // doc should exist
+    return db.get(remoteDoc._id).should.be.fulfilledWith({ _id: remoteDoc._id, _rev: remoteDoc._rev, data: remoteDoc.data })
+  })
 
-    let err = null
-    doc = null
-
-    try {
-      // check the db to look for the new doc
-      doc = await db.get(docId)
-    } catch (e) {
-      err = e
-      err.status.should.be.exactly(404)
-    } finally {
-      (doc === null).should.be.true
-    }
-
-    await new Promise(resolve => setTimeout(resolve, 5 * 1000))
-
-    doc = null
-    err = null
-
-    try {
-      // after a certain amount of time, doc should exist
-      doc = await db.get(docId)
-    } catch (e) {
-      err = e
-    } finally {
-      (err === null).should.be.true
-      doc.should.be.type('object')
-      doc._id.should.equal(docId)
-      doc.test.should.equal(smallDoc.test)
-    }
-    count.should.be.exactly(1)
-  }).timeout(7 * 1000)
+  it('can sync and stop sync a database', async function () {
+    // create a database
+    const db = cozy.client.offline.createDatabase(DOCTYPE, { adapter: 'memory' })
+    // create a doc
+    const sampleDoc = { data: 'some Data' }
+    const remoteDoc = await cozy.client.data.create(DOCTYPE, sampleDoc)
+    // check the db to look for the new doc
+    db.get(remoteDoc._id).should.be.rejectedWith({ message: 'missing' })
+    // activate synchronisation every 1000ms
+    cozy.client.offline.startSync(DOCTYPE, 0.1)
+    // after a certain amount of time, doc should exist
+    await sleep(300)
+    cozy.client.offline.stopAllSync()
+    // create another doc after sync
+    const anotherDoc = { data: 'some other Data' }
+    const anotherRemoteDoc = await cozy.client.data.create(DOCTYPE, anotherDoc)
+    const promises = []
+    promises.push(db.get(remoteDoc._id).should.be.fulfilledWith({ _id: remoteDoc._id, _rev: remoteDoc._rev, data: remoteDoc.data }))
+    promises.push(db.get(anotherRemoteDoc._id).should.be.rejectedWith({ message: 'missing', status: 404 }))
+    return Promise.all(promises)
+  })
 })
