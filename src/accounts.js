ef1f26a1b4d64867dc9a035b4f51a1daf1b315eb
Grégory Le Garec <gregory.legarec@gmail.com>
2017-04-26 18:50:28 +0200

    refactor: replace login and password parameters with auth object :sparkles:

--- src/accounts.js
+++ src/accounts.js
@@ -1,13 +1,13 @@
 import {cozyFetchJSON} from './fetch'
 
-export function create (cozy, konnector, login = '', password = '', name = '') {
-  return cozyFetchJSON(cozy, 'POST', '/data/io.cozy.accounts', {
+export function create (cozy, konnector, auth, name = '') {
+  return cozyFetchJSON(cozy, 'POST', '/data/io.cozy.accounts/', {
     name: name,
     account_type: konnector.vendorLink,
     status: 'PENDING',
     auth: {
-      login: login,
-      password: password
+      login: auth.login,
+      password: auth.password
     }
   })
 }
