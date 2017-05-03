0a57bfe67f93f49e5199066c29c998d50f068bdd
Enguerran <enguerran@ticabri.com>
2017-05-03 17:05:49 +0200

    feat: remove referenced files

--- test/integration/relations.js
+++ test/integration/relations.js
@@ -10,11 +10,20 @@ const COZY_STACK_URL = process.env && process.env.COZY_STACK_URL || ''
 const COZY_STACK_VERSION = process.env && process.env.COZY_STACK_VERSION
 const COZY_STACK_TOKEN = process.env && process.env.COZY_STACK_TOKEN
 
+async function createRandomFile (cozy, random) {
+  return cozy.client.files.create('datastring1', {
+    name: 'foo_' + random(),
+    contentType: 'application/json'
+  })
+}
+
 describe('references', async function () {
   let random
   const cozy = {}
+  let specialDoc
+  let ids = []
 
-  beforeEach(function () {
+  before(async function () {
     if (COZY_STACK_VERSION === '2') {
       this.skip()
     }
@@ -23,25 +32,28 @@ describe('references', async function () {
       token: COZY_STACK_TOKEN
     })
     random = randomGenerator()
-  })
-
-  it('bind files to a doc', async function () {
-    const ids = []
     for (let i = 0; i < 3; i++) {
-      let file = await cozy.client.files.create('datastring1', {
-        name: 'foo_' + random(),
-        contentType: 'application/json'
-      })
+      let file = await createRandomFile(cozy, random)
       ids.push(file._id)
     }
 
-    const doc = await cozy.client.data.create('io.cozy.testreferencer', {
+    specialDoc = await cozy.client.data.create('io.cozy.testreferencer', {
       name: 'foo_' + random()
     })
 
-    await cozy.client.data.addReferencedFiles(doc, ids)
+    await cozy.client.data.addReferencedFiles(specialDoc, ids)
+  })
 
-    const ids2 = await cozy.client.data.listReferencedFiles(doc)
-    ids2.should.eql(ids)
+  it('bind files to a doc', async function () {
+    const anotherDoc = await createRandomFile(cozy, random)
+    await cozy.client.data.addReferencedFiles(specialDoc, anotherDoc._id)
+    const ids2 = await cozy.client.data.listReferencedFiles(specialDoc)
+    ids2.should.eql(ids.concat(anotherDoc._id))
+  })
+
+  it('remove references', async function () {
+    await cozy.client.data.removeReferencedFiles(specialDoc, ids[0])
+    const ids2 = await cozy.client.data.listReferencedFiles(specialDoc)
+    ids2.should.not.containEql(ids[0])
   })
 })
